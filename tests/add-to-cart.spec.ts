04d2c9bd722f11f4a5c60b0e419181456168f01a
msanthosh87 <msanthosh87@gmail.com>
2026-03-01 02:37:38 +0530

    added new test TC-010 in addtocart

--- tests/add-to-cart.spec.ts
+++ tests/add-to-cart.spec.ts
@@ -16,3 +16,13 @@ test('TC-002 get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('TC-010 get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
