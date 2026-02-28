cd2a4f87d679fb5a328d90140f20ce3b997720f2
msanthosh87 <msanthosh87@gmail.com>
2026-03-01 02:27:47 +0530

    changes 3

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -37,3 +37,13 @@ test('TC-006 get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
+
+test('TC-007 get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
