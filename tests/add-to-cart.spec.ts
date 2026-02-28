6ea3babe966c552318fa52fa92190cf875df0f55
msanthosh87 <msanthosh87@gmail.com>
2026-02-28 18:20:48 +0530

    added more validations

--- tests/add-to-cart.spec.ts
+++ tests/add-to-cart.spec.ts
@@ -2,9 +2,9 @@ import { test, expect } from '@playwright/test';
 
 test('TC-001 has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page).toHaveURL(/.*intro/);
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
 test('TC-002 get started link', async ({ page }) => {
