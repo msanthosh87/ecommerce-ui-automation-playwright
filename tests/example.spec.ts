14fcd78e27a18de97fa05b2e9d76c71e8222a4a9
msanthosh87 <msanthosh87@gmail.com>
2026-03-01 02:20:41 +0530

    changes 1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('TC-003 has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('TC-004 get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
